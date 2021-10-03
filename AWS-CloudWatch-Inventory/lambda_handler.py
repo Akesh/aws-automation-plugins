0c58c266bbc09cb79770bf612ac60ba607cb4be3
akesh patil <akesh.patil@blazeclan.com>
2021-10-03 21:17:24 +0530

    Code fix in lambda handler

--- AWS-CloudWatch-Inventory/lambda_handler.py
+++ AWS-CloudWatch-Inventory/lambda_handler.py
@@ -43,53 +43,8 @@ def bytes_2_human_readable(number_of_bytes):
 
 
 def get_cloudwatch_data(event, context):
-    logger.info("Event {}".format(event))import logging
-
-import boto3
-
-cwlogs_client = boto3.client('logs')
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-HTML_CONTENT_START = "<!DOCTYPE><html><head><title>CloudWatch Report</title><style>table{font-family:arial,sans-serif;border:1px solid #dddddd;width:100%;} h2{font-family:arial,sans-serif;width:100%;text-align:center;} h3{font-family:arial,sans-serif;width:100%;padding-left:25%;} td,th{border:1px solid #dddddd;text-align:left; padding:8px;} tr:nth-child(even){background-color:#dddddd;}</style></head><body>"
-HTML_CONTENT_END = "</body></html>"
-
-
-def bytes_2_human_readable(number_of_bytes):
-    if number_of_bytes < 0:
-        raise ValueError("!!! number_of_bytes can't be smaller than 0 !!!")
-
-    step_to_greater_unit = 1024.
-
-    number_of_bytes = float(number_of_bytes)
-    unit = 'bytes'
-
-    if (number_of_bytes / step_to_greater_unit) >= 1:
-        number_of_bytes /= step_to_greater_unit
-        unit = 'KB'
-
-    if (number_of_bytes / step_to_greater_unit) >= 1:
-        number_of_bytes /= step_to_greater_unit
-        unit = 'MB'
-
-    if (number_of_bytes / step_to_greater_unit) >= 1:
-        number_of_bytes /= step_to_greater_unit
-        unit = 'GB'
-
-    if (number_of_bytes / step_to_greater_unit) >= 1:
-        number_of_bytes /= step_to_greater_unit
-        unit = 'TB'
-
-    precision = 1
-    number_of_bytes = round(number_of_bytes, precision)
-
-    return str(number_of_bytes) + ' ' + unit
-
-
-def get_cloudwatch_data(event, context):
     logger.info("Event {}".format(event))
-    cwlogs_list_response = cwlogs_client.describe_log_groups()    
+    cwlogs_list_response = cwlogs_client.describe_log_groups()
     if cwlogs_list_response['logGroups']:
         html_content = "<table><tr><th>Log Group</th><th>Data Size</th>"
         while True:
